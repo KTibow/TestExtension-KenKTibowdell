1610bdef68d75f961fce9f4ad19a8eed05cb69fd
Kendell <KTibow@users.noreply.github.com>
2015-09-13 10:30:54 -0700

    Change

---
+++ Kendell_Scratchx_Extension_One.js
@@ -0,0 +1,12 @@
+(function(extIn) {
+    var InfoIn = {
+        blocks: [
+
+        ],
+        menus: {
+
+        },
+        url: 'http://ktibow.github.io/TestExtension-KenKTibowdell/Extensions/Kendell_Scratchx_Extension_One.js'
+    };
+    ScratchExtensions.register('Test Extension', InfoIn, extIn);
+}
